c9b90dfae4ca089bf100a466de34add21f0d444b
jpmitiguy <jpm.mitiguy@gmail.com>
2022-11-01 13:20:06 -0700

    from vscode 11-1-22 1:20 PM

--- settings.py
+++ settings.py
@@ -6,17 +6,21 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
 # variables
+    # window settings
 WIDTH = 360
 HEIGHT = 480
 FPS = 30
+    # player settings
 STARTING_COLOR = GREEN
 PLAYER_FRIC = 0.18
 PLAYER_GRAV = 1
-COINS = 25
-MOB_FRIC = 0.05
-ENEMY_FRIC = 0.45
-MOB_COLOR = BLUE
-ENEMY_COLOR = RED
 SCORE = 0
 WIN = -1
+    # coin/mob settings
+COINS = 25
+MOB_FRIC = 0.05
+MOB_COLOR = BLUE
+    # enemy settings
+ENEMY_FRIC = 0.45
+ENEMY_COLOR = RED
 ENEMY_SPEED_INC = ENEMY_FRIC/COINS
